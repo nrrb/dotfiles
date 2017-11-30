27042ca718e3e5b7c918a4163ff133c15372e485
Nick Bennett <nick271828@gmail.com>
2017-11-30 13:29:37 -0600

    Cleanup, remove deprecated files.

--- .vim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim/
-git submodule update --init
-git clone https://github.com/tothebeat/powerline-fonts.git ~/.fonts/
-fc-cache -vf ~/.fonts
-echo Change your font in the terminal program!
